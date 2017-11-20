e23f2a3a6383c231bdf4ea9a9a21530ae7c62f25
UOBChemEng <33328494+UOBChemEng@users.noreply.github.com>
2017-11-20 21:19:24 +0000

    Add files via upload

---
+++ js/UOBChemEngMain-0.0.1.js
@@ -0,0 +1,58 @@
+//Make a vertical array of tabs
+function openVtab(evt, vtabName) 
+	{
+    var i, vtabcontent, vtablinks;
+    vtabcontent = document.getElementsByClassName("vtabcontent");
+    for (i = 0; i < vtabcontent.length; i++) 
+    	{
+        vtabcontent[i].style.display = "none";
+    	}
+    vtablinks = document.getElementsByClassName("vtablinks");
+    for (i = 0; i < vtablinks.length; i++) 
+    	{
+        vtablinks[i].className = vtablinks[i].className.replace(" active", "");
+    	}
+    document.getElementById(vtabName).style.display = "block";
+    evt.currentTarget.className += " active";
+	}
+
+//Remove tags from User input and data stores
+function sanitize(html) 
+	{
+	var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+	var tagOrComment = new RegExp(
+		'<(?:'
+		// Comment body.
+		+ '!--(?:(?:-*[^->])*--+|-?)'
+		// Special "raw text" elements whose content should be elided.
+		+ '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+		+ '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+		// Regular name
+		+ '|/?[a-z]'
+		+ tagBody
+		+ ')>',
+		'gi');
+	var oldHtml;
+	do 
+		{
+		oldHtml = html;
+		html = html.replace(tagOrComment, '');
+		} while (html !== oldHtml);
+  	return html.replace(/</g, '&lt;');
+	}		
+	
+function getSpreadsheetUrl(sheetKey,sheetGID)
+	{
+	var details = {	
+		Minutes_SSC_2017: {
+						key: '1G8Sa9qhqZS0xuSD5hxYF3lRtz2tJc-Gv8GIqVzfa4p0',
+						MasterSheet: 0,
+						SortKeys: 1809433302
+						}
+		};
+	var ssUrl1 = "https://docs.google.com/spreadsheets/d/"
+	var ssUrl2 = "/edit#gid=";
+	return ssUrl1+details[sheetKey]['key']+ssUrl2+details[sheetKey][sheetGID];
+	
+	}
+	
